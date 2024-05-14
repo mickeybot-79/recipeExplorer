79debde40f004435204bf22b2e9fe97fe8317308
mickeybot-79 <91163243+mickeybot-79@users.noreply.github.com>
2024-05-14 12:46:10 -0300

    Update usersApiSlice.js

--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -1,5 +1,6 @@
 import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice"
+import { logOut, setCredentials } from "./authSlice"
 
 const usersAdapter = createEntityAdapter({})
 
