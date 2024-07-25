a30f67433719566d6bae55ea9db8e23ff6f92051
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-07-25 20:56:55 -0300

    commented manifest

--- src/hooks/useOnUnsaved.js
+++ src/hooks/useOnUnsaved.js
@@ -3,36 +3,36 @@ import { useBeforeUnload, unstable_useBlocker as useBlocker } from "react-router
 
 function usePrompt(message, { beforeUnload } = {}) {
 
-let blocker = useBlocker(
-  React.useCallback(
-     () => (typeof message === "string" ? !window.confirm(message) : false),
-  [message]
-  )
-);
-let prevState = React.useRef(blocker.state);
+  let blocker = useBlocker(
+    React.useCallback(
+      () => (typeof message === "string" ? !window.confirm(message) : false),
+      [message]
+    )
+  );
+  let prevState = React.useRef(blocker.state);
 
-React.useEffect(() => {
+  React.useEffect(() => {
     if (blocker.state === "blocked") {
-    blocker.reset();
-  }
-  prevState.current = blocker.state;
-}, [blocker]);
+      blocker.reset();
+    }
+    prevState.current = blocker.state;
+  }, [blocker]);
 
-useBeforeUnload(
-     React.useCallback(
-       (event) => {
-         if (beforeUnload && typeof message === "string") {
+  useBeforeUnload(
+    React.useCallback(
+      (event) => {
+        if (beforeUnload && typeof message === "string") {
           event.preventDefault();
           event.returnValue = message;
-         }
+        }
       },
       [message, beforeUnload]
     ),
-  { capture: true }
- );
+    { capture: true }
+  );
 }
 
 export function Prompt({ when, message, ...props }) {
-    usePrompt(when ? message : false, props);
-    return null;
+  usePrompt(when ? message : false, props);
+  return null;
 }
